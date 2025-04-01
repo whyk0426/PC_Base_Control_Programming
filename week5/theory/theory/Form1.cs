aff822d9344235cac51ddc7860791640eedd5833
whyk0426 <kkyyss426@gmail.com>
2025-04-01 17:41:48 +0900

    week5

---
+++ week5/theory/theory/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace theory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calcul.Divide(10, 3, out int q, out int _);
+            Console.WriteLine(q);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = 2, y =4;
+            calcul.Swap(ref x, ref y);
+        }
+    }
+}
---
+++ week5/theory/theory/calcul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theory
+{
+    internal class calcul
+    {
+        public static void Divide (int a, int b, out int quo, out int remain)
+        {
+            quo = a / b;
+            remain = a & b;
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }
+    }
+}
